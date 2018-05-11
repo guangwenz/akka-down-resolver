51d166217618df614784719a61109ccf05c2fedb
Guangwen Zhou <zgwmike@hotmail.com>
2018-05-11 00:39:05 -0700

    added travis for site doc

--- scripts/decrypt-keys.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-openssl aes-256-cbc -K $encrypted_2129fb3f7525_key -iv $encrypted_2129fb3f7525_iv -in travis-deploy-key.enc -out travis-deploy-key -d;
-chmod 600 travis-deploy-key;
-cp travis-deploy-key ~/.ssh/id_rsa;
--- src/multi-jvm/scala/org/guangwenz/akka/cluster/TestTags.scala
+++
@@ -1,5 +0,0 @@
-package org.guangwenz.akka.cluster
-
-import org.scalatest.Tag
-
-object TestTags extends Tag("long-running")
